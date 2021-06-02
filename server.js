166edf8ac7a3afb861e3e98351937046bc9e6ac6
gonzalovsilva <gonzalosilva.info@gmail.com>
2021-06-02 14:50:47 +0200

    separate express routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
 //load env vars
 dotenv.config({
   path: './config/config.env'
@@ -8,6 +11,9 @@ dotenv.config({
 
 const app = express()
 
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+ 
 const PORT = process.env.PORT || 5000
 
 app.listen(
